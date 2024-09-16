125913d68de4adcd2dfd13dba4f5038d0145d7a9
OscarVelazquezA <micheloe08@gmail.com>
2024-09-15 18:40:21 -0700

    Update navbar.blade.php

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -2,7 +2,7 @@
 
 <nav class="border-gray-200 bg-black dark:bg-gray-800 dark:border-gray-700">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="{{ route('dashboard') }}">
+        <a  wire:navigate href="/"">
             <x-application-mark class="block h-9 w-auto" />
         </a>
       <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
