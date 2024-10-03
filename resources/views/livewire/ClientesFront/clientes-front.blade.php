24bbe9b81f85a92cc88901bf76580aa069bb4911
OscarVelazquezA <micheloe08@gmail.com>
2024-10-03 10:33:35 -0700

    Update clientes-front.blade.php

--- resources/views/livewire/ClientesFront/clientes-front.blade.php
+++ resources/views/livewire/ClientesFront/clientes-front.blade.php
@@ -36,7 +36,7 @@
 
 </div>
 <div class="grid gap-12 mb-12 md:grid-cols-1">
-    <button type="submit" class="text-white bg-gradient-to-r from-gray-400 via-gray-600 to-gray-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-gray-300 dark:focus:ring-gray-800 font-small rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 w-2" wire:click ="buscarNumeroTel()">
+    <button type="submit" class="text-white bg-gradient-to-r from-gray-200 via-gray-400 to-gray-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-gray-300 dark:focus:ring-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" wire:click ="buscarNumeroTel()">
         Buscar Datos 🔎
      </button>
 </div>
